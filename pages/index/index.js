6eeba0333fb6f8f9e5accadba9a291b33e3b56b3
782686224@qq.com <782686224@qq.com>
2019-11-15 09:26:15 +0800

    首次提交

---
+++ pages/index/index.js
@@ -0,0 +1,71 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    a1src: '/icon/a1.png',
+    a2src: '/icon/a2.png',
+    a3src: '/icon/a3.png',
+    a4src: '/icon/a4.png',
+    imgUrls: ['cloud://xiaochengxu-h51fa.7869-xiaochengxu-h51fa-1300639849/images/1573699321190m.png',
+      'cloud://xiaochengxu-h51fa.7869-xiaochengxu-h51fa-1300639849/images/1573693179946m.png',
+      'cloud://xiaochengxu-h51fa.7869-xiaochengxu-h51fa-1300639849/images/1573697027625m.png']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
